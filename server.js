7f92dab503d38d07b04aa9055b790420995f199e
Himanshubhadoria11 <himanshubhadoria22@gmail.com>
2025-07-16 23:31:34 +0530

    changes

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const port = process.env.PORT || 5200;
 //   credentials: true
 // }))
 app.use(cors({
-  origin: 'http://localhost:5173', // allow your frontend origin
+  origin: 'https://youtube-frontend-psi-liart.vercel.app/', // allow your frontend origin
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 }));
